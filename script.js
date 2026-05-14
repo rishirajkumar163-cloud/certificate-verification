aec35cf2189fffce58645ec2da0a2ba201894e77
rishirajkumar163-cloud <rishirajkumar163@gmail.com>
2026-05-14 08:31:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ get
 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyBPe74d1Em1nPcxYbBMiS1S2Wob7NloMcE",
-    authDomain: "certificate-verification-386d8.firebaseapp.com",
-    databaseURL: "https://certificate-verification-386d8-default-rtdb.firebaseio.com",
-    projectId: "certificate-verification-386d8",
-    storageBucket: "certificate-verification-386d8.firebasestorage.app",
-    messagingSenderId: "324706708399",
-    appId: "1:324706708399:web:e2b5a85cc59d047fc81d5a",
-    measurementId: "G-CZGCRJZSLX"
-  };
+  apiKey: "AIzaSyBPe74d1Em1nPcxYbBMiS1S2Wob7NloMcE",
+  authDomain: "certificate-verification-386d8.firebaseapp.com",
+  databaseURL: "https://certificate-verification-386d8-default-rtdb.firebaseio.com",
+  projectId: "certificate-verification-386d8",
+  storageBucket: "certificate-verification-386d8.firebasestorage.app",
+  messagingSenderId: "324706708399",
+  appId: "1:324706708399:web:e2b5a85cc59d047fc81d5a",
+  measurementId: "G-CZGCRJZSLX"
+};
 
 const app =
 initializeApp(firebaseConfig);
